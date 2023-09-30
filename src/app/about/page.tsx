66628bd01baf7a89bc3a868a93aab63eac555b98
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2023-09-30 21:05:28 +0300

    add navbar and pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,14 +1,15 @@
 import Navbar from "@/app/comp/Navbar";
 
-export default function Home() {
+export default function about() {
     return (
         <>
             <Navbar></Navbar>
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
                 <h1 className="text-6xl font-bold">
-                    When Logic Meets Design </h1>
+                   About Me </h1>
             </main>
         </>
 
     )
 }
+
--- src/app/comp/Navbar.tsx
+++ src/app/comp/Navbar.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
-  return (
-    <div className="header">
-      <h1>Header</h1>
-    </div>
-  );
+    return (
+        <nav className="bg-blue-500 p-4">
+            <div className="container">
+                <div className="mx-auto">
+                    <Link href="/">TUTDEV</Link>
+                </div>
+                <div className="mx-auto mt-4">
+                    <Link href="/projects">Projects</Link>
+                    <Link href="/about">About</Link>
+                </div>
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
