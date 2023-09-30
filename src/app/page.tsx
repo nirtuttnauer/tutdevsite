7cfd6268701e5e735686c41f452df28c7eb6bb1e
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2023-09-30 23:51:47 +0300

    three js added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
-import Navbar from "@/app/comp/Navbar";
+import ThreeScene from "@/app/ThreeSchene";
 
 export default function Home() {
     return (
         <>
-            <Navbar></Navbar>
-            <main className="flex min-h-screen flex-col items-center justify-between p-24">
-                <h1 className="text-6xl font-bold">
-                    When Logic Meets Design </h1>
-            </main>
-        </>
 
+            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                <h1 className="text-6xl font-bold z-10">
+                    When Logic Meets Design </h1>
+                <div className="z-0">
+                          <ThreeScene/>
+                </div>
+
+            </main>
+
+        </>
     )
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,8 @@
 import Navbar from "@/app/comp/Navbar";
 
-export default function projects() {
+export default function Projects() {
     return (
         <>
-            <Navbar></Navbar>
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
                 <h1 className="text-6xl font-bold">
                     My Projects </h1>
