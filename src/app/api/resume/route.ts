a78b94bd28e3bdcfcaac454baeb285b612a0b830
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2023-10-06 01:54:14 +0300

    update the resume

--- src/app/api/resume/route.ts
+++ src/app/api/resume/route.ts
@@ -1,22 +1,8 @@
+import resume from './resume.json';
 
-export async function GET(req: Request) {
-    return new Response('hello world', {status: 200})
-}
+// You can convert the JavaScript object to JSON using JSON.stringify
+const resumeJSON = JSON.stringify(resume, null, 2);
 
-export async function POST(req: Request) {
-    let body = await req.json()
-    console.log(body)
-    return new Response('hello post', {status: 200})
-}
-
-export async function PUT(req: Request) {
-    let body = await req.json()
-    console.log(body)
-    return new Response('hello put', {status: 200})
-}
-
-export async function DELETE(req: Request) {
-    let body = await req.json()
-    console.log(body)
-    return new Response('hello delete', {status: 200})
+export async function GET() {
+    return new Response(resumeJSON, {status: 200})
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,14 +1,55 @@
 import Navbar from "@/app/comp/Navbar";
 
+
 export default function Projects() {
     return (
         <>
             <main className="flex min-h-screen flex-col items-center justify-between p-24">
                 <h1 className="text-6xl font-bold">
                     My Projects </h1>
+                {projectsGallery()}
             </main>
         </>
 
     )
 }
 
+function projectsGallery() {
+    return (
+        <>
+            <div className="flex flex-row">
+                <div className="p-4">
+                    <p>test</p>
+                </div>
+                <div className="p-4">
+                    <p>test</p>
+                </div>
+                <div className="p-4">
+                    <p>test</p>
+                </div>
+            </div>
+            <div className="flex flex-row">
+                <div className="p-4">
+                    <p>test</p>
+                </div>
+                <div className="p-4">
+                    <p>test</p>
+                </div>
+                <div className="p-4">
+                    <p>test</p>
+                </div>
+            </div>
+            <div className="flex flex-row">
+                <div className="p-4">
+                    <p>test</p>
+                </div>
+                <div className="p-4">
+                    <p>test</p>
+                </div>
+                <div className="p-4">
+                    <p>test</p>
+                </div>
+            </div>
+        </>
+    )
+}
