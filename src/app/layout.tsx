f23c711ab894d59be888a82ca63afb1bd1b6eef4
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2023-10-06 16:24:49 +0300

    made the code shorter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import Navbar from "@/app/comp/Navbar";
 import Footer from "@/app/comp/Footer";
+import {Toaster, toast} from 'sonner'
 
 const inter = Inter({subsets: ['latin']})
 
@@ -15,11 +16,13 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={inter.className}>
+        <Toaster/>
         <Navbar/>
+        {/*<button onClick={() => toast('My first toast')}>*/}
         {children}
         <Footer/>
         </body>
 
         </html>
-    )
+)
 }
